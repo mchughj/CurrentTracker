a47c6c32923a7afe0d49fafa8b543584c3052aca
Jason McHugh <mchughj@gmail.com>
2020-02-19 12:53:09 -0800

    Working average counter
    This is a complete implementation of an "duration adaptive average calculator".   It is a container class where you add items to it along with the time that the items were seen.  Then you can ask for an average value from a given point in time and for a specific duration.  If we have sufficient datapoints to cover the duration (as defined by the oldest datapoint and the duration of time) then we compute the average.
    
    The data structure is maintained as a circular array of fixed size.

--- CTFirmware/CTFirmware.ino
+++ CTFirmware/CTFirmware.ino
@@ -16,6 +16,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1351.h>
 #include <SPI.h>
+#include <stdarg.h>
 
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 128 
@@ -42,6 +43,24 @@ Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_CS, SCREEN_DC, SCREEN_RST);
 // Largest unsigned long int which is a 4 byte integer.
 #define ULONG_MAX 0xFFFFFFFF
 
+// The Arduino Zero has two serial ports.  To get output for debugging I use
+// the Serial rather than Serial.
+#define Serial SerialUSB
+
+void myPrintf(const char * format, ...)
+{
+  char buf[256];
+  int len;
+
+  va_list ap;
+  va_start(ap, format);
+
+  len = vsnprintf(buf, 256, format, ap);
+  Serial.print(buf);
+
+  va_end(ap);
+}
+
 class CurrentDatapoints {
 public:
   CurrentDatapoints(int numberToTrack);
@@ -49,7 +68,8 @@ public:
   void addData(unsigned long int timeMicros, uint8_t numberOfInterrupts);
 
   // This method will return -1 if there is no good data to support 
-  double getApproximateUsage( unsigned long int currentMicros, unsigned long int microDuration );
+  double getApproximateAverage( unsigned long int currentMicros, unsigned long int microDuration );
+  double getOverallAverage();
  
 private:
   unsigned long int getElapsed( unsigned long int earlierMicros, unsigned long int laterMicros);
@@ -62,7 +82,7 @@ private:
   // Circular dynamically allocated array
   Datapoint *d;
   int allocatedDatapoints;
-  int headIndex;
+  int nextIndex;
   int numberDatapoints;
 
   unsigned long int lastMicrosSeen;
@@ -75,10 +95,11 @@ private:
 CurrentDatapoints::CurrentDatapoints(int numberToTrack) { 
   allocatedDatapoints = numberToTrack;
   numberDatapoints = 0;
-  headIndex = 0;
+  nextIndex = 0;
   d = new Datapoint[allocatedDatapoints];
   memset( d, 0, sizeof(Datapoint)*allocatedDatapoints);
   lastMicrosSeen = 0;
+  totalSeconds = 0;
 }
 
 
@@ -89,69 +110,127 @@ void CurrentDatapoints::addData(unsigned long int timeMicros, uint8_t numberOfIn
   }
   totalInterrupts += numberOfInterrupts;
   lastMicrosSeen = timeMicros;
+  myPrintf( "addData - updated overall;  timeMicros: %lu, numberOfInterrupts: %d, "
+    "totalInterrupts: %d, totalSeconds: %d\n", 
+    timeMicros, numberOfInterrupts, totalInterrupts, (int)totalSeconds);
 
-  if (numberDatapoints == allocatedDatapoints) { 
-    d[headIndex].timeMicros = timeMicros;
-    d[headIndex].numberOfInterrupts = numberOfInterrupts;
-  } else {
-    d[numberDatapoints].timeMicros = timeMicros;
-    d[numberDatapoints].numberOfInterrupts = numberOfInterrupts;
+  if (numberDatapoints < allocatedDatapoints) { 
     numberDatapoints++;
   }
-  headIndex++;
-  if (headIndex == allocatedDatapoints) { 
-    headIndex = 0;
+  d[nextIndex].timeMicros = timeMicros;
+  d[nextIndex].numberOfInterrupts = numberOfInterrupts;
+
+  myPrintf( "addData - done;  timeMicros: %lu, numberOfInterrupts: %d, index: %d, numberDatapoints: %d\n", 
+    timeMicros, numberOfInterrupts, nextIndex, numberDatapoints);
+
+  // nextIndex always points one past where the last datapoint was added.  In
+  // other words it always is the next place where a new datapoint will be
+  // added.
+  nextIndex++;
+  if (nextIndex == allocatedDatapoints) { 
+    nextIndex = 0;
   }
 }
 
 double
-CurrentDatapoints::getApproximateUsage( unsigned long int currentMicros, unsigned long int microDuration ) {
+CurrentDatapoints::getApproximateAverage( unsigned long int currentMicros, unsigned long int microDuration ) {
   unsigned long int maxPastTimeMicros = 0;
   uint8_t interruptsAccumulated = 0;
 
-  int index = headIndex;
+  myPrintf( "getApproximateAverage - beginning; currentMicros: %lu, "
+            "microDuration: %lu, numberDatapoints: %d\n", 
+            currentMicros, microDuration, numberDatapoints);
+
+  int index = nextIndex;
+  int dataPointCount = 0;
   for (int i=0; i<numberDatapoints; i++) { 
     index = index - 1;
     if (index < 0) { 
       index = allocatedDatapoints - 1;
     }
     unsigned long int elapsedMicros = getElapsed(d[index].timeMicros, currentMicros);
+    myPrintf( "getApproximateAverage - examining item; index: %d, "
+                   "currentMicros: %lu, itemTimeMicros: %lu, elapsed: %lu\n",
+                   index, currentMicros, d[index].timeMicros, elapsedMicros);
     if (elapsedMicros < microDuration) { 
       maxPastTimeMicros = d[index].timeMicros;
       interruptsAccumulated += d[index].numberOfInterrupts;
+      dataPointCount++;
     } else {
       break;
     }
   }
-
-  if (maxPastTimeMicros == 0) { 
+  myPrintf( "getApproximateAverage - found index;  currentMicros: %lu, "
+                 "microDuration: %lu, nextIndex: %d, index: %d, dataPointCount: %d, "
+                 "maxPastTimeMicros: %lu, interruptsAccumulated:%d\n", 
+                 currentMicros, microDuration, nextIndex, index,
+                 dataPointCount, maxPastTimeMicros, interruptsAccumulated);
+  if (dataPointCount == 0) { 
+    myPrintf( "getApproximateAverage - dataPointCount is zero;\n");
     return -1;
   }
 
+  // Get the time distance between the currentMicros and the oldest one 
+  // that we found. If this is not sufficiently close to the duration 
+  // that I am looking for then don't compute the average. 
+  // 
+  // This does create the behavior where if we stop getting samples (no
+  // battery is consumed) then the average value will 'migrate' from low
+  // frequency durations (which will have data insufficiency due to no recent
+  // samples) to higher ones because the duration captured will naturally get
+  // longer and longer until it captures even the largest microDuration.
+  // Since there are no datapoints the average will be very small and dwindle
+  // to zero.  This is valid as we aren't requiring a minimum number of
+  // datapoints to participate in the average but instead ensuring that we 
+  // have enough data.  It also produces the right results as when we 
+  // stop consuming battery then the larger durations will still show an
+  // average value but the smaller ones will not.
   unsigned long int totalDurationMicros = getElapsed(maxPastTimeMicros, currentMicros);
   if (totalDurationMicros < microDuration * 0.85) { 
+    myPrintf( "getApproximateAverage - returning insufficient data; "
+              "microDuration: %lu, totalDurationMicros: %lu, difference: %lu\n", 
+              microDuration, totalDurationMicros, 
+              (microDuration - totalDurationMicros));
     return -1;
   }
 
   double secondsBetweenInterrupts = ((double)totalDurationMicros)/1000000.0;
-  return (614.4 * interruptsAccumulated)/secondsBetweenInterrupts; 
+  double result = (614.4 * interruptsAccumulated)/secondsBetweenInterrupts; 
+  myPrintf( "getApproximateAverage - returning result;  currentMicros: %lu, "
+                 "microDuration: %lu, result: %d\n", 
+                 currentMicros, microDuration, (int)result);
+  return result;
 }
 
+double 
+CurrentDatapoints::getOverallAverage() {
+  if (totalSeconds==0) { 
+    return -1;
+  }
+  return (614.4 * totalInterrupts)/totalSeconds; 
+}
 
 // Return the number of elapsed microseconds that have occurred between time
 // laterMicros and earlierMicros.  
 unsigned long int 
 CurrentDatapoints::getElapsed( unsigned long int earlierMicros, unsigned long int laterMicros) {
+  unsigned long int result;
   // earlierMicros is assumed to the be the 'earlier' time but time may have wrapped
   // around.
   if (earlierMicros > laterMicros) { 
     // We have wrapped around since earlierMicros is supposed to be less.  
     // So it looks like this from a timeline perspective:
     //   0  |-----laterMicros---------earlierMicros-----| (ULONG_MAX)
-    return ( ULONG_MAX - earlierMicros + laterMicros );
+    result = ( ULONG_MAX - earlierMicros + laterMicros );
   } else {
-    return laterMicros - earlierMicros;
+    result = laterMicros - earlierMicros;
   }
+#ifdef DEBUG_ELAPSED_TIME
+  myPrintf( "getElapsed - done;  earlierMicros: %lu, laterMicros: %lu, "
+            "isEarlier: %d, result: %lu\n", 
+    earlierMicros, laterMicros, (earlierMicros < laterMicros) ? 1 : 0, result);
+#endif
+  return result;
 }
 
 
@@ -164,7 +243,7 @@ CurrentDatapoints::getElapsed( unsigned long int earlierMicros, unsigned long in
 // of how much current has been consumed.  
 unsigned long int startZeroInterruptMicros;
 volatile unsigned long int mostRecentInterruptMicros;
-volatile double numberOfInterrupts;
+volatile uint8_t numberOfInterrupts;
 
 // Every time the interrupt fires we know that 0.17607 milli amps, or 170.6 micro amps, 
 // have been consumed from the battery.  
@@ -176,6 +255,8 @@ unsigned int totalNumberOfInterrupts;
 
 double averagemAh;
 double totalConsumedmAh;
+
+CurrentDatapoints c(200);
 
 void interruptForCoulomb() {
   mostRecentInterruptMicros = micros();
@@ -210,10 +291,11 @@ void setup() {
   Serial.println("setup complete!");
 }
 
-double secondsBetweenInterrupts;
+double secondsBetweenInterrupts = 0;
 
 void loop() {
   if (numberOfInterrupts) {
+    c.addData(micros(), numberOfInterrupts);
 
     secondsBetweenInterrupts = (mostRecentInterruptMicros-startZeroInterruptMicros)/1000000.0;
     averagemAh = (614.4 * numberOfInterrupts)/secondsBetweenInterrupts; 
@@ -242,6 +324,18 @@ void loop() {
   tft.println(" secs)");
   tft.print("Total mAh: ");
   tft.println(totalConsumedmAh);
+  tft.println("---");
+  tft.print("C-Overall: ");
+  tft.println(c.getOverallAverage());
+  unsigned long int currentMicros = micros();
+  tft.print("C-20s: ");
+  tft.println(c.getApproximateAverage(currentMicros, 20 * 1000 * 1000));
+  tft.print("C-60s: ");
+  tft.println(c.getApproximateAverage(currentMicros, 60 * 1000 * 1000));
+  tft.print("C-120s: ");
+  tft.println(c.getApproximateAverage(currentMicros, 120 * 1000 * 1000));
+  tft.print("C-360s: ");
+  tft.println(c.getApproximateAverage(currentMicros, 360 * 1000 * 1000));
   delay(2000);
 }
 
