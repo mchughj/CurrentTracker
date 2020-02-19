329f0d804801b0378afae5a9ab4627a3f5832120
Jason McHugh <mchughj@gmail.com>
2020-02-18 21:02:44 -0800

    Tracking many datapoints with approx averages
    This diff introduces a new class that will maintain a circular array of information about the interrupts that are fired and when.  It then supports a generic mechanism to get the number of interrupts that have occurred for arbitary (but bounded by unsigned long in -- about 1 hour) time periods.
    
    This is a first pass and still needs debugging and testing.

--- CTFirmware/CTFirmware.ino
+++ CTFirmware/CTFirmware.ino
@@ -5,7 +5,7 @@
 // track the current used by a project.  
 //
 // When programming this ensure that the board selected is 
-// Arduino Zero (Native USB Port).
+//   "Arduino Zero (Native USB Port)"
 //
 
 // Required libraries:
@@ -38,6 +38,123 @@ Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_CS, SCREEN_DC, SCREEN_RST);
 #define MAGENTA         0xF81F
 #define YELLOW          0xFFE0  
 #define WHITE           0xFFFF
+
+// Largest unsigned long int which is a 4 byte integer.
+#define ULONG_MAX 0xFFFFFFFF
+
+class CurrentDatapoints {
+public:
+  CurrentDatapoints(int numberToTrack);
+
+  void addData(unsigned long int timeMicros, uint8_t numberOfInterrupts);
+
+  // This method will return -1 if there is no good data to support 
+  double getApproximateUsage( unsigned long int currentMicros, unsigned long int microDuration );
+ 
+private:
+  unsigned long int getElapsed( unsigned long int earlierMicros, unsigned long int laterMicros);
+
+  typedef struct {
+    unsigned long int timeMicros;
+    uint8_t numberOfInterrupts;
+  } Datapoint;
+
+  // Circular dynamically allocated array
+  Datapoint *d;
+  int allocatedDatapoints;
+  int headIndex;
+  int numberDatapoints;
+
+  unsigned long int lastMicrosSeen;
+
+  double totalSeconds;
+  unsigned long int totalInterrupts;
+};
+
+
+CurrentDatapoints::CurrentDatapoints(int numberToTrack) { 
+  allocatedDatapoints = numberToTrack;
+  numberDatapoints = 0;
+  headIndex = 0;
+  d = new Datapoint[allocatedDatapoints];
+  memset( d, 0, sizeof(Datapoint)*allocatedDatapoints);
+  lastMicrosSeen = 0;
+}
+
+
+void CurrentDatapoints::addData(unsigned long int timeMicros, uint8_t numberOfInterrupts) {
+  if (lastMicrosSeen != 0) { 
+    unsigned long int microsElapsed = getElapsed(lastMicrosSeen, timeMicros);
+    totalSeconds += ((double)microsElapsed) / 1000000.0;
+  }
+  totalInterrupts += numberOfInterrupts;
+  lastMicrosSeen = timeMicros;
+
+  if (numberDatapoints == allocatedDatapoints) { 
+    d[headIndex].timeMicros = timeMicros;
+    d[headIndex].numberOfInterrupts = numberOfInterrupts;
+  } else {
+    d[numberDatapoints].timeMicros = timeMicros;
+    d[numberDatapoints].numberOfInterrupts = numberOfInterrupts;
+    numberDatapoints++;
+  }
+  headIndex++;
+  if (headIndex == allocatedDatapoints) { 
+    headIndex = 0;
+  }
+}
+
+double
+CurrentDatapoints::getApproximateUsage( unsigned long int currentMicros, unsigned long int microDuration ) {
+  unsigned long int maxPastTimeMicros = 0;
+  uint8_t interruptsAccumulated = 0;
+
+  int index = headIndex;
+  for (int i=0; i<numberDatapoints; i++) { 
+    index = index - 1;
+    if (index < 0) { 
+      index = allocatedDatapoints - 1;
+    }
+    unsigned long int elapsedMicros = getElapsed(d[index].timeMicros, currentMicros);
+    if (elapsedMicros < microDuration) { 
+      maxPastTimeMicros = d[index].timeMicros;
+      interruptsAccumulated += d[index].numberOfInterrupts;
+    } else {
+      break;
+    }
+  }
+
+  if (maxPastTimeMicros == 0) { 
+    return -1;
+  }
+
+  unsigned long int totalDurationMicros = getElapsed(maxPastTimeMicros, currentMicros);
+  if (totalDurationMicros < microDuration * 0.85) { 
+    return -1;
+  }
+
+  double secondsBetweenInterrupts = ((double)totalDurationMicros)/1000000.0;
+  return (614.4 * interruptsAccumulated)/secondsBetweenInterrupts; 
+}
+
+
+// Return the number of elapsed microseconds that have occurred between time
+// laterMicros and earlierMicros.  
+unsigned long int 
+CurrentDatapoints::getElapsed( unsigned long int earlierMicros, unsigned long int laterMicros) {
+  // earlierMicros is assumed to the be the 'earlier' time but time may have wrapped
+  // around.
+  if (earlierMicros > laterMicros) { 
+    // We have wrapped around since earlierMicros is supposed to be less.  
+    // So it looks like this from a timeline perspective:
+    //   0  |-----laterMicros---------earlierMicros-----| (ULONG_MAX)
+    return ( ULONG_MAX - earlierMicros + laterMicros );
+  } else {
+    return laterMicros - earlierMicros;
+  }
+}
+
+
 
 // Track two micro times.  The first, startZeroInterruptMicros, is set during the main
 // routine, outside of an interrupt, when the numberOfInterrupts is set to zero.  
